38b753af8a0757601e737c970c6dd80dd98551f7
1F10230071 <s1f102300717@iniad.org>
2024-07-18 14:06:38 +0900

    メモ欄に関連するコードを追加しました。

--- todo/views.py
+++ todo/views.py
@@ -9,7 +9,8 @@ from todo.models import Task
 def index(request):
     if request.method == 'POST':
         task = Task(title=request.POST['title'],
-                    due_at=make_aware(parse_datetime(request.POST['due_at'])))
+                    due_at=make_aware(parse_datetime(request.POST['due_at'])),
+                    memo = request.POST.get('memo' ,''),)   #空欄を容認
         task.save()
 
     if request.GET.get('order') == 'due':
@@ -43,6 +44,7 @@ def update(request, task_id):
     if request.method == 'POST':
         task.title = request.POST['title']
         task.due_at = make_aware(parse_datetime(request.POST['due_at']))
+        task.memo = request.POST['memo']
         task.save()
         return redirect(detail, task_id)
     context = {
